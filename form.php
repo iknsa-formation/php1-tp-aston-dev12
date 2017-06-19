3a85a55bdf4ec429e30323ec64a6b4577703576f
Khalid Sookia <khalidsookia@gmail.com>
2017-06-19 17:14:13 +0200

    initialised repo

---
+++ form.php
@@ -0,0 +1,100 @@
+<?php
+
+$results = [];
+$res = 0;
+
+foreach ($_POST as $key => $value) {
+	// J'initialise le type d'opération et les autres variables que je vais utilisé
+	$op = '';
+
+	if (preg_match('/nombre/', $key) > 0) {
+		$number = $value;
+
+		$results[substr($key, -1)]['number'] = $number;
+	}
+
+	// var_dump($key, $value);
+	// Je vérifie si le champ est un nombre ou une opération
+	if (preg_match('/op/', $key) > 0) {
+		// Je suis sur un type de champ opération
+
+		$operateurLogique = 'op' . substr($key, -1);
+		$operateur = $_POST[$operateurLogique];
+		$results[substr($key, -1)]["operateur"] = $operateur;
+
+		if ($res === 0) {
+			$nombrePrecedent = 'nombre' . (substr($key, -1));
+			$res = $_POST[$nombrePrecedent];
+		}
+
+		$nombreSuivant = 'nombre' . (substr($key, -1) + 1);
+		$next = $_POST[$nombreSuivant];
+
+		switch ($operateur) {
+			case '+':
+				$res += $next;
+				break;
+			case '-':
+				$res -= $next;
+				break;
+			case '*':
+				$res = $res * $next;
+				break;
+			case '/':
+				$res = $res / $next;
+				break;
+			case '%':
+				$res = $res % $next;
+				break;
+			
+			default:
+				die('operateur pas permis');
+				break;
+		};
+	}
+}
+echo "<pre>";
+var_dump($res);
+echo "</pre>";
+
+echo "<hr><pre>";
+var_dump($results);
+echo "</pre>";
+$title = "Le traitement du formulaire précedent";
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Form traitement</title>
+</head>
+<body>
+<h1><?= strtoupper($title) ?></h1>
+<h2><?= substr($title, 15) ?></h2>
+
+<h2><?php $explodedTitle = explode('u', $title); ?></h2>
+
+<ul>
+	<?php foreach($explodedTitle as $value): ?>
+		<li><?= $value ?></li>
+	<?php endforeach; ?>
+</ul>
+
+<p>
+<?php
+/**
+
+	Le résultat de l'opération 
+	$nombre1 (<?= $nombre1 ?>) opération: (<?= $op1 ?>) et$nombre2 (<?= $nombre2 ?>) est $res1 (<?= $res1 ?>)
+	<br>
+	Le résultat de l'opération 
+	$nombre2 (<?= $nombre2 ?>) opération: (<?= $op2 ?>) et$nombre3 (<?= $nombre3 ?>) est $res2 (<?= $res2 ?>)
+
+*/
+?>
+	...
+</p>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP</title>
+</head>
+<body>
+
+<h1>My great APP</h1>
+
+<form method="POST" action="form.php">
+	<div>
+		<label for="nombre1">Nombre 1</label>
+		<input type="text" name="nombre1" id="nombre1">
+	</div>
+	<span><input type="text" name="op1"></span>
+	<div>
+		<label for="nombre2">Nombre 2</label>
+		<input type="text" name="nombre2" id="nombre2">
+	</div>
+	<span><input type="text" name="op2"></span>
+	<div>
+		<label for="nombre3">Nombre 3</label>
+		<input type="text" name="nombre3" id="nombre3">
+	</div>
+	<span><input type="text" name="op3"></span>
+	<div>
+		<label for="nombre4">Nombre 4</label>
+		<input type="text" name="nombre4" id="nombre4">
+	</div>
+	<span><input type="text" name="op4"></span>
+	<div>
+		<label for="nombre5">Nombre 5</label>
+		<input type="text" name="nombre5" id="nombre5">
+	</div>
+
+	<div>
+		<input type="submit">
+	</div>
+</form>
+
+</body>
+</html>
